4148b1ca45574ad24ed7889ed666e2f9fe46a844
djwalshy <the_walshs@att.net>
2015-02-02 18:24:47 -0600

    Fix action

---
+++ Customer Relationship System/Main.vb
@@ -0,0 +1,10 @@
+﻿Public Class Main
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'CustomersDataSet1.Customer_Info' table. You can move, or remove it, as needed.
+        Me.Customer_InfoTableAdapter.Fill(Me.CustomersDataSet1.Customer_Info)
+        'TODO: This line of code loads data into the 'CustomersDataSet1.Customer_Info' table. You can move, or remove it, as needed.
+        Me.Customer_InfoTableAdapter.Fill(Me.CustomersDataSet1.Customer_Info)
+
+    End Sub
+End Class
